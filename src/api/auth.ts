007d2cc8b80c46f6ed8126d99961107b92bbbf5b
Rey Halsall <reduhq25@gmail.com>
2025-02-23 14:39:51 -0600

    Adding a password confirmacion in the register form

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,14 +1,22 @@
 import { supabase } from "./supabaseClient";
 
-export const signUpNewUser = async (email:string, password: string)=>{
-    const {data, error} = await supabase.auth.signUp({
-        email,
-        password,
-        options:{emailRedirectTo:`${process.env.NEXT_PUBLIC_HOST_URL}/confirm-account`}
-    },)
-    if(error){
-        console.error('There was a problem signing up ', error)
-        return {success: false, error}
-    }
-    return {success: true, data}
-}
+export const signUpNewUser = async (
+  email: string,
+  password: string,
+  name: string
+) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_HOST_URL}/confirm-account`,
+      data: { name },
+    },
+  });
+  if (error) {
+    console.error("There was a problem signing up ", error);
+    return { success: false, error };
+  }
+  console.log({ success: true, data })
+  return { success: true, data };
+};
--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -16,22 +16,29 @@ import {
   FormLabel,
   FormMessage,
 } from "./ui/form";
-// import { zodResolver } from "@hookform/resolvers/zod"
-let  passwordErrors:{message: string}[] = []
+
+let passwordErrors: { message: string }[] = [];
 const formSchema = z
   .object({
-    email: z.string().min(1, {message: 'El email es obligatorio'}).email({ message: "Email inválido" }),
-    password: z
-      .string(),
-    // confirmPassword: z.string(),
+    name: z.string().min(3, {message: 'Ingresa un nombre válido'}),
+    email: z
+      .string()
+      .min(1, { message: "El email es obligatorio" })
+      .email({ message: "Email inválido" }),
+    password: z.string(),
+    confirmPassword: z.string(),
   })
   .superRefine((data, ctx) => {
-    passwordErrors = []
+    passwordErrors = [];
     if (data.password.length < 8) {
-      passwordErrors.push({ message: "La contraseña debe tener al menos 8 caracteres" });
+      passwordErrors.push({
+        message: "La contraseña debe tener al menos 8 caracteres",
+      });
     }
     if (!/[a-z]/.test(data.password)) {
-      passwordErrors.push({ message: "Debe contener al menos una letra minúscula" });
+      passwordErrors.push({
+        message: "Debe contener al menos una letra minúscula",
+      });
     }
     if (!/[A-Z]/.test(data.password)) {
       passwordErrors.push({ message: "Debe contener al menos una mayúscula" });
@@ -40,7 +47,9 @@ const formSchema = z
       passwordErrors.push({ message: "Debe contener al menos un número" });
     }
     if (!/[!@#$%^&*()_+\-=[\]{};:'"|<>?,.\/`~\\]/.test(data.password)) {
-      passwordErrors.push({ message: "Debe contener al menos un carácter especial" });
+      passwordErrors.push({
+        message: "Debe contener al menos un carácter especial",
+      });
     }
 
     passwordErrors.forEach((error) => {
@@ -51,13 +60,13 @@ const formSchema = z
       });
     });
 
-    // if (data.password !== data.confirmPassword) {
-    //   ctx.addIssue({
-    //     code: z.ZodIssueCode.custom,
-    //     message: "Las contraseñas no coinciden",
-    //     path: ["confirmPassword"],
-    //   });
-    // }
+    if (data.password !== data.confirmPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Las contraseñas no coinciden",
+        path: ["confirmPassword"],
+      });
+    }
   });
 
 export function RegisterForm({
@@ -67,14 +76,15 @@ export function RegisterForm({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       email: "",
       password: "",
-      // confirmPassword: "",
+      confirmPassword: "",
     },
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    const response = await signUpNewUser(values.email, values.password);
+    const response = await signUpNewUser(values.email, values.password, values.name);
 
     if (response.success) {
       if (response.data?.user?.identities?.length == 0) {
@@ -121,6 +131,21 @@ export function RegisterForm({
           <div className="grid gap-2">
             <FormField
               control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombre</FormLabel>
+                  <FormControl>
+                    <Input placeholder="John Doe" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
@@ -148,13 +173,13 @@ export function RegisterForm({
               render={({ field }) => (
                 <FormItem>
                   <div className="flex items-center">
-                  <FormLabel>Contraseña</FormLabel>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-4 hover:underline"
-                  >
-                    Olvidaste tu contraseña?
-                  </a>
+                    <FormLabel>Contraseña</FormLabel>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Olvidaste tu contraseña?
+                    </a>
                   </div>
                   <FormControl>
                     <Input type="password" placeholder="" {...field} />
@@ -165,11 +190,28 @@ export function RegisterForm({
                         ? passwordErrors.map((error, index) => (
                             <li key={index}>{error.message}</li>
                           ))
-                        : [form.formState.errors.password].map((error, index) => (
-                            <li key={index}>{error?.message}</li>
-                          ))}
+                        : [form.formState.errors.password].map(
+                            (error, index) => (
+                              <li key={index}>{error?.message}</li>
+                            )
+                          )}
                     </ul>
                   )}
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Coonfirmar contraseña</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -177,20 +219,6 @@ export function RegisterForm({
           <Button type="submit" className="w-full">
             Crear Cuenta
           </Button>
-          {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-        <Button variant="outline" className="w-full">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-              fill="currentColor"
-            />
-          </svg>
-          Login with GitHub
-        </Button> */}
         </div>
         <div className="text-center text-sm">
           Ya tienes cuenta?{" "}
