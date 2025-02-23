6f39ebfc2732b29acef4dcb1fcc81d212ae6c205
Rey Halsall <reduhq25@gmail.com>
2025-02-22 23:11:49 -0600

    Redirecting in the confirm-account view

--- src/app/(auth)/confirm-account/page.tsx
+++ src/app/(auth)/confirm-account/page.tsx
@@ -1,22 +1,29 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const ConfirmAccount = () => {
-  const [paramsObject, setParamsObject] = useState<Record<string, string> | null>(null);
+  const router = useRouter();
+  const [paramsObject, setParamsObject] = useState<Record<
+    string,
+    string
+  > | null>(null);
 
   useEffect(() => {
     // Este código solo se ejecutará en el navegador
     const hash = window.location.hash.substring(1);
     const params = Object.fromEntries(new URLSearchParams(hash));
+    if (Object.keys(params).length === 0) {
+      return router.push(process.env.NEXT_PUBLIC_HOST_URL as string);
+    }
     setParamsObject(params);
-  }, []);
-  
-  if(paramsObject == null){
-    return (
-      <div>Cargando...</div>
-    )
+  }, [router]);
+  console.log(paramsObject);
+
+  if (paramsObject == null) {
+    return <div>Cargando...</div>;
   }
 
   if (paramsObject.error) {
